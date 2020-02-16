59884db31f3dc5f3a124a6ac5b15e65e52c86df9
gilmartaj <44650363+gilmartaj@users.noreply.github.com>
2020-02-16 08:21:34 +0000

    Add files via upload

--- Linguagem Java/0.0.1-d/src/remid/Remid.java
+++ Linguagem Java/0.0.1-d/src/remid/Remid.java
@@ -1,9 +1,25 @@
 package remid;
 
+import uk.co.caprica.vlcj.component.AudioMediaPlayerComponent;
+import uk.co.caprica.vlcj.player.MediaPlayer;
+
 public class Remid
 {
+
 	public static void main(String[] args)
 	{
-		
+		try
+		{
+			MediaPlayer reprodutor = new AudioMediaPlayerComponent().getMediaPlayer();
+			reprodutor.playMedia(args[0]);
+			while(!reprodutor.isPlaying());
+			while(reprodutor.isPlaying());
+			System.out.println("O áudio terminou.");
+			System.exit(0);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Erro ao reproduzir.");
+		}
 	}
 }
