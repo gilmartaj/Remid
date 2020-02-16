cb06bb2c3effdd5ca238889ddc0863bcedf83d72
gilmartaj <44650363+gilmartaj@users.noreply.github.com>
2020-02-16 07:21:06 +0000

    Add files via upload

---
+++ Linguagem C/0.0.1-d/src/remid.c
@@ -0,0 +1,20 @@
+#include "../../include/SDL.h"
+#include "../../include/SDL_mixer.h"
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{	
+	int erro = 0;
+	SDL_Init(SDL_INIT_AUDIO);
+	Mix_OpenAudio( 44100, MIX_DEFAULT_FORMAT, 2, 1024);
+	Mix_Music *mus = Mix_LoadMUS(argv[1]);
+	erro = Mix_PlayMusic( mus, 1 );
+	if(erro == -1)
+	{
+		printf("Erro ao reproduzir.\n");
+		exit(1);
+	}
+	while(Mix_PlayingMusic());
+	printf("O áudio terminou de tocar\n");
+}
